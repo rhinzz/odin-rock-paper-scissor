7ebd75149add9ebc7f116d2c8427a4bfcf6baac7
rhinzz <prama.sudjana@gmail.com>
2025-04-21 10:58:53 +0700

    Refactoring script.js and modifying html element

--- script.js
+++ script.js
@@ -8,7 +8,6 @@ buttons.forEach(button => {
     });
 })
 
-const resultContainer = document.querySelector("#resultContainer");
 const result = document.querySelector("#result");
 
 function getComputerChoice() {
@@ -23,21 +22,32 @@ function getComputerChoice() {
     }
 }
 
+let humanScore = 0;
+let computerScore = 0;
 
 function playRound(humanChoice, computerChoice) {
     if (humanChoice == "paper" && computerChoice == "rock") {
         result.innerText = "You win! Paper beats rock!";
+        humanScore++;
     } else if (humanChoice == "rock" && computerChoice == "paper") {
         result.innerText = "You lose! Paper beats rock!";
+        computerScore++;
     } else if (humanChoice == "rock" && computerChoice == "scissor") {
         result.innerText = "You win! Rock beats scissor!";
+        humanScore++;
     } else if (humanChoice == "scissor" && computerChoice == "rock") {
         result.innerText = "You lose! Rock beats scissor!";
+        computerScore++;
     } else if (humanChoice == "scissor" && computerChoice == "paper") {
         result.innerText = "You win! Scissor beats paper!";
+        humanScore++;
     } else if (humanChoice == "paper" && computerChoice == "scissor") {
         result.innerText = "You lose! Scissor beats paper!";
+        computerScore++;
     } else {
         result.innerText = "Tie!";
     }
+
+    document.querySelector('#human').innerText = `${humanScore}`;
+    document.querySelector('#computer').innerText = `${computerScore}`;
 }
